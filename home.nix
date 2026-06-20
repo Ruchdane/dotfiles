191135bd87d197d1050a126c323702dcc7392eb6
ruchdane <ruchdaneabiodun@gmail.com>
2026-06-20 18:54:08 +0200

    add devenv

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
     home.packages = with pkgs; [
+    devenv
     # General CLI tools
     lolcat
     wget
