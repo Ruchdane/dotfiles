311d2a2d85d3ab4ee301234816f06cbf43a2aaed
ruchdane <ruchdaneabiodun@gmail.com>
2026-08-10 00:42:35 +0200

    update flake input

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@ in
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  nixpkgs.config.allowUnfree = true;
   # The home.packages option allows you to install Nix packages into your
   # environment.
     home.packages = with pkgs; [
@@ -68,6 +69,8 @@ in
     corepack
 
     opencode
+    gemini-cli
+    antigravity
   ] ++ lib.optionals (!isWSL) [
     neofetch
     droidcam
